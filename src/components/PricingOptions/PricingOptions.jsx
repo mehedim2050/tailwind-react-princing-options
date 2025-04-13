bc4f89ecaf5dd5b793ac56f20ac45fc570242020
mehedim2050 <mehedi.m2050@gmail.com>
2025-04-13 16:48:27 +0600

    added the all content

--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,7 @@
 import React, { use } from 'react';
-import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricing from '../DaisyPricing/DaisyPricing';
+// import PricingCard from '../PricingCard/PricingCard';
+
 const PricingOptions = ({ pricingPromise }) => {
 const pricingData = use(pricingPromise);
 
@@ -8,12 +10,18 @@ const pricingData = use(pricingPromise);
         <div>
                  <h2 className='text-5xl'>Get our Membership</h2>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-                {
+                {/* {
                     pricingData.map(pricing=><PricingCard
                         key={pricing.id}
                         
                         pricing={pricing}></PricingCard>)
+                } */}
+                {
+                    pricingData.map(pricing=> <DaisyPricing 
+                          key={pricing.id}
+                        pricing={pricing}></DaisyPricing>)
                 }
+               
             </div>
         </div>
     );
